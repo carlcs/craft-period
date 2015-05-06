ef1882148e3db069fb1297dfd18e64c126a796ca
carlcs <seelbach@gmx.de>
2015-05-06 21:51:26 +0200

    Initial commit

---
+++ dateperiod/DatePeriodPlugin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Craft;
+
+class DatePeriodPlugin extends BasePlugin
+{
+	function getName()
+	{
+		return 'Date Period';
+	}
+
+	function getVersion()
+	{
+		return '1.0';
+	}
+
+	function getDeveloper()
+	{
+		return 'carlcs';
+	}
+
+	function getDeveloperUrl()
+	{
+		return 'https://github.com/carlcs/craft-dateperiod';
+	}
+}
